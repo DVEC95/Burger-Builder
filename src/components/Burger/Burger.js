743f903782102d6d8eba015880381dc40625e8f6
David Eastcroft <davideastcroft95@gmail.com>
2020-10-30 17:04:45 +0000

    Adds state for ingredients and logic for dynamic output of ingredients.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,17 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = (props) => {
+const burger = ( props ) => {
+	const ingredients = Object.keys(props.ingredients)
+											.map(key => {
+												return [...Array(props.ingredients[key])].map((_, i) => {
+													return <BurgerIngredient key={key + i} type={key} />
+												});
+											});										
 	return (
 		<div className={classes.burger}>
 			<BurgerIngredient type="bread-top" />
-			<BurgerIngredient type="cheese" />
-			<BurgerIngredient type="meat" />
+			{ ingredients }
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	);
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,14 +3,24 @@ import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+	state = {
+		ingredients: {
+			salad: 1,
+			bacon: 2,
+			cheese: 1,
+			meat: 2
+		}
+	};
+
 	render () {
 		return (
 			<Fragment>
-				<Burger />
+				<Burger ingredients={this.state.ingredients}/>
 				<div>Build Controls</div>
 			</Fragment>
 		);
-	}
-}
+	};
+};
 
 export default BurgerBuilder;
