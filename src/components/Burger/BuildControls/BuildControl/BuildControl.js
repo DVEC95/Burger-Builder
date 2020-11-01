8a4e530e18eea3900a66971fe62015753bb3e209
David Eastcroft <davideastcroft95@gmail.com>
2020-11-01 13:38:02 +0000

    Adds display of controls for ingredients.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from './BuildControl.modules.css';
+import classes from './BuildControl.module.css';
 
 const buildControl = ( props ) => (
 	<div className={ classes.buildControl }>
